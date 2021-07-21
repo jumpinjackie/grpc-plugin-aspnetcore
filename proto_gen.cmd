2e948d286b21c1a6ccb9a0c9169e2e3598e46010
Jackie Ng <jumpinjackie@gmail.com>
2021-07-21 18:39:00 +1000

    Parameterize the triplet in proto_gen.cmd so windows triplets other than x64-windows can be used (provided the required tools/libs have been built for it)

--- proto_gen.cmd
+++ proto_gen.cmd
@@ -1,6 +1,8 @@
 @echo off
-set PROTOBUF_INCLUDE=%CD%\thirdparty\vcpkg-export\installed\x64-windows\include
-set PROTOBUF_TOOLS=%CD%\thirdparty\vcpkg-export\installed\x64-windows\tools\protobuf
-set GRPC_TOOLS=%CD%\thirdparty\vcpkg-export\installed\x64-windows\tools\grpc
+set TRIPLET=%1
+if %TRIPLET% == "" then set TRIPLET=x64-windows
+set PROTOBUF_INCLUDE=%CD%\thirdparty\vcpkg-export\installed\%TRIPLET%\include
+set PROTOBUF_TOOLS=%CD%\thirdparty\vcpkg-export\installed\%TRIPLET%\tools\protobuf
+set GRPC_TOOLS=%CD%\thirdparty\vcpkg-export\installed\%TRIPLET%\tools\grpc
 echo [grpc]: Protoc annotations
 %PROTOBUF_TOOLS%\protoc.exe -Iproto -I%PROTOBUF_INCLUDE% --cpp_out=src/plugin --plugin=protoc-gen-grpc=%GRPC_TOOLS%\grpc_cpp_plugin.exe proto/aspnetcore.proto
